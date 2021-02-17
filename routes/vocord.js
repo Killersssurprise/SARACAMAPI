8a882c9b73dd0a278f9ba5ba1aeb40a885368dbd
TheAmbidexter <33318001+TheAmbidexter@users.noreply.github.com>
2021-02-17 21:49:07 +0300

    Update vocord.js

--- routes/vocord.js
+++ routes/vocord.js
@@ -42,7 +42,7 @@ router.post("/", jsonParser, function (req, res) {
 
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
-                
+                // Получает из ответа общее кол-во проездов
                 let s = '{"passages":' + JSON.stringify(JSON.parse(body).TotalItems) + '}';
                 console.log(s);
                 res.send(s);
@@ -75,6 +75,7 @@ router.post("/", jsonParser, function (req, res) {
 
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
+            // Получает из ответа общее кол-во нарушений
                 let s = '{"violations":' + JSON.stringify(JSON.parse(body).TotalItems) + '}';
                 console.log(s);
                 res.send(s);
@@ -105,6 +106,7 @@ router.post("/", jsonParser, function (req, res) {
 
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
+                // Получает из ответа активна ли камера
                 let s = '{"Active":' + JSON.stringify(JSON.parse(body).CamerasDto[0]["DbCamera"]["IsVisible"]) + '}';
                 console.log(s);
                 res.send(s);
