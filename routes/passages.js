55123e08bfb23c62816a1b992407a027133c52f4
Killersssurprise <killersssurprise@gmail.com>
2021-02-18 14:06:41 +0300

    Changed port

--- routes/passages.js
+++ routes/passages.js
@@ -3,153 +3,175 @@ var router = express.Router();
 
 var http = require('http');
 const jsonParser = express.json();
+var kordon_driver = require('/drivers/kordon_driver');
 
 var login = "admin";
-var password="Fk5bu8jG";
+var password = "Fk5bu8jG";
 var job = "getStats";
-var timestampStart=1612818000;
-var timestampEnd=1612891239;
-var ip='192.168.72.11';
+var timestampStart = 1612818000;
+var timestampEnd = 1612891239;
+var ip = '192.168.72.11';
+var device_type = '192.168.72.11';
 
-/* GET users listing. */
-/*router.post('/', function(req, res) {
-    res.setHeader('Content-Type', 'application/json');
-    //res.set("Content-Type", "application/json");
-
-    var start_time = req.param('start_time');
-    var end_time = req.param('end_time');
-    var login = req.param('login');
-    var password=  req.param('password');
-    var ip =  req.param('ip');
-
-    console.log("ip: "+ip);
-    console.log("login: "+login);
-    console.log("password: "+password);
-    console.log("start_time: "+start_time);
-    console.log("end_time: "+end_time);
-
-    //magic
-
-    res.send(getFakeJson());
-
-});*/
+const kordonID = '2';
+const orlanID = '3';
+const vocordID = '5';
 
 router.post("/", jsonParser, function (req, res) {
 
     var st = req.param('start_time');
     var et = req.param('end_time');
     var lg = req.param('login');
-    var ps=  req.param('password');
-    var i =  req.param('ip');
+    var ps = req.param('password');
+    var i = req.param('ip');
+    var dt = req.param('device_type');
 
-    if (typeof lg !== 'undefined' && lg !== null){
+
+    if (typeof lg !== 'undefined' && lg !== null) {
         login = lg;
-    }else{
-        let s = '{"error":'+"no param login"+'}';
+    } else {
+        let s = '{"error":' + "no param login" + '}';
         res.send(s);
         return;
     }
 
-    if (typeof ps !== 'undefined' && ps !== null){
+    if (typeof ps !== 'undefined' && ps !== null) {
         password = ps;
-    }else{
-        let s = '{"error":'+"no param password"+'}';
+    } else {
+        let s = '{"error":' + "no param password" + '}';
         res.send(s);
         return;
     }
 
-    if (typeof st !== 'undefined' && st !== null){
+    if (typeof st !== 'undefined' && st !== null) {
         timestampStart = st;
-    }else{
-        let s = '{"error":'+"no param start_time"+'}';
+    } else {
+        let s = '{"error":' + "no param start_time" + '}';
         res.send(s);
         return;
     }
 
-    if (typeof et !== 'undefined' && et !== null){
+    if (typeof et !== 'undefined' && et !== null) {
         timestampEnd = et;
-    }else{
-        let s = '{"error":'+"no param end_time"+'}';
+    } else {
+        let s = '{"error":' + "no param end_time" + '}';
         res.send(s);
         return;
     }
 
-    if (typeof ip !== 'undefined' && ip !== null){
+    if (typeof ip !== 'undefined' && ip !== null) {
         ip = i;
-    }else{
-        let s = '{"error":'+"no param ip"+'}';
+    } else {
+        let s = '{"error":' + "no param ip" + '}';
         res.send(s);
         return;
     }
 
+    // if (typeof dt !== 'undefined' && dt !== null){
+    device_type = dt;
+    // }else{
+    //     let s = '{"error":'+"no param device_type"+'}';
+    //     res.send(s);
+    //     return;
+    // }
 
-    var data = JSON.stringify({ "auth": {
-            "login": login,
-            "password": password
-        },
-        "request": {
-            "job": job,
-            "getStats": {
-                "timestampStart": timestampStart,
-                "timestampEnd": timestampEnd,
-                "speedThresholds": [
-                    {
-                        "name": "Превышение на 20", "min": 23, "max": 43
-                    },
-                    {
-                        "name": "Превышение на 40",
-                        "min": 43, "max": 63
-                    },
-                    {
-                        "name": "Превышение на 60", "min": 63, "max": 83
-                    },
-                    {
-                        "name": "Превышение на 80", "min": 83, "max": 0
-                    }
-                ],
-                "showInfo": true
-            }
-        }
-    });
+    //test code
 
-    var options = {
-        host: ip,
-        path: '/api11.php',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Content-Length': Buffer.byteLength(data)
-        }
-
-    };
     var answ='';
-    try {
-        var httpreq = http.request(options, function (response) {
-            //response.setEncoding('utf8');
-            response.on('data', function (chunk) {
-                //console.log(chunk);
-                answ += chunk;
-            }).on('end', function () {
-                var str = JSON.parse(answ);
 
-                let s = '{"passages":' + JSON.stringify(str['getStats']['common']['total']) + '}';
-                console.log(s);
-                res.send(s);
-            }).on('error', (err) => {
-                let s = '{"status":' + "inactive" + '}';
-                res.send(s);
-                console.error(err.stack);
-            });
-        });
+    switch (a) {
+        case null:
 
-        httpreq.write(data);
-        httpreq.end();
-
-    } catch (e) {
-
-        console.log(e);
+            answ = kordon_driver.getPassagesData(login, password, ip, timestampStart, timestampEnd);
+            break;
+        case 'undefined':
+            answ = kordon_driver.getPassagesData(login, password, ip, timestampStart, timestampEnd);
+            break;
+        case kordonID:
+            answ = kordon_driver.getPassagesData(login, password, ip, timestampStart, timestampEnd);
+            break;
+        case vocordID:
+            answ = '{"error":' + "no vocord driver yet" + '}';
+            break;
+        case orlanID:
+            answ = '{"status":' + "no orlan driver yet" + '}';
+            break;
+        default:
+            answ = kordon_driver.getPassagesData(login, password, ip, timestampStart, timestampEnd);
+            break;
     }
-    //res.send(data);
+
+    res.send(answ);
+
+
+//     var data = JSON.stringify({
+//         "auth": {
+//             "login": login,
+//             "password": password
+//         },
+//         "request": {
+//             "job": job,
+//             "getStats": {
+//                 "timestampStart": timestampStart,
+//                 "timestampEnd": timestampEnd,
+//                 "speedThresholds": [
+//                     {
+//                         "name": "Превышение на 20", "min": 23, "max": 43
+//                     },
+//                     {
+//                         "name": "Превышение на 40",
+//                         "min": 43, "max": 63
+//                     },
+//                     {
+//                         "name": "Превышение на 60", "min": 63, "max": 83
+//                     },
+//                     {
+//                         "name": "Превышение на 80", "min": 83, "max": 0
+//                     }
+//                 ],
+//                 "showInfo": true
+//             }
+//         }
+//     });
+//
+//     var options = {
+//         host: ip,
+//         path: '/api11.php',
+//         method: 'POST',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'Content-Length': Buffer.byteLength(data)
+//         }
+//
+//     };
+//     var answ = '';
+//     try {
+//         var httpreq = http.request(options, function (response) {
+//             //response.setEncoding('utf8');
+//             response.on('data', function (chunk) {
+//                 //console.log(chunk);
+//                 answ += chunk;
+//             }).on('end', function () {
+//                 var str = JSON.parse(answ);
+//
+//                 let s = '{"passages":' + JSON.stringify(str['getStats']['common']['total']) + '}';
+//                 console.log(s);
+//                 res.send(s);
+//             }).on('error', (err) => {
+//                 let s = '{"status":' + "inactive" + '}';
+//                 res.send(s);
+//                 console.error(err.stack);
+//             });
+//         });
+//
+//         httpreq.write(data);
+//         httpreq.end();
+//
+//     } catch (e) {
+//
+//         console.log(e);
+//     }
+//     //res.send(data);
 });
 
 module.exports = router;
