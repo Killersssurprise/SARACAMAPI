a7626ad7f71f063dd09de64fdd2a8af79be57570
Killersssurprise <killersssurprise@gmail.com>
2021-02-19 09:02:05 +0300

    Added orlan request stuff

--- drivers/orlan_driver.js
+++ drivers/orlan_driver.js
@@ -63,7 +63,7 @@ module.exports = {
                         newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
                     }
 
-                    if (data[i].Key == 'Recog24Hours') {
+                    if (data[i].Key == 'Recog1Hour') {
                         // violations_count = value;
                         let s = '{"violations":' + data[i].Value + '}';
                         res.send(s);
@@ -90,74 +90,111 @@ module.exports = {
             headers: headers
         };
 
-        var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
-        var data = JSON.parse(b);
+        // var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
+        // var data = JSON.parse(b);
+        //
+        //
+        // // var data = JSON.parse(body);
+        //
+        // var newarr = [];
+        // data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
+        // newarr = '{';
+        // for (let i = 0; i < data.length; i++) {
+        //     if (i === data.length - 1) {
+        //         newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
+        //     } else {
+        //         newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
+        //     }
+        //
+        //     if (data[i].Key == 'Recog24Hours') {
+        //         // violations_count = value;
+        //         let s = '{"passages":' + data[i].Value + '}';
+        //         res.send(s);
+        //     }
+        // }
 
+        function callback(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(body);
 
-        // var data = JSON.parse(body);
+                var data = JSON.parse(body);
+                var newarr = [];
+                data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
+                newarr = '{';
+                for (let i = 0; i < data.length; i++) {
+                    if (i === data.length - 1) {
+                        newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
+                    } else {
+                        newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
+                    }
 
-        var newarr = [];
-        data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
-        newarr = '{';
-        for (let i = 0; i < data.length; i++) {
-            if (i === data.length - 1) {
-                newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
+                    if (data[i].Key == 'Recog24Hours') {
+                        // violations_count = value;
+                        let s = '{"violations":' + data[i].Value + '}';
+                        res.send(s);
+                    }
+                }
+
             } else {
-                newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
-            }
-
-            if (data[i].Key == 'Recog24Hours') {
-                // violations_count = value;
-                let s = '{"violations":' + data[i].Value + '}';
-                res.send(s);
+                console.error(error);
+                res.send(error);
             }
         }
 
-        // function callback(error, response, body) {
-        //     if (!error && response.statusCode === 200) {
-        //         console.log(body);
-        //
-        //         //var values = body;
-        //         var violations_count;
-        //
-        //         //just for test
-        //         var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
-        //         var data = JSON.parse(b);
-        //
-        //
-        //         // var data = JSON.parse(body);
-        //
-        //         var newarr = [];
-        //         data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
-        //         newarr = '{';
-        //         for (let i = 0; i < data.length; i++) {
-        //             if (i === data.length - 1) {
-        //                 newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
-        //             } else {
-        //                 newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
-        //             }
-        //
-        //             if (data[i].Key == 'Recog24Hours') {
-        //                 violations_count = value;
-        //                 let s = '{"violations":' + data[i].Value + '}';
-        //                 res.send(s);
-        //             }
-        //         }
-        //
-        //     } else {
-        //         console.error(error);
-        //         res.send(error);
-        //     }
-        // }
-        //
-        // request(options, callback);
+        request(options, callback);
 
     },
 
     getIsActiveData: function getIsActiveData(login, password, ip, timestampStart, timestampEnd, res) {
 
-        let s = '{"status":' + "active" + '}';
-        res.send(s);
+
+        var url = 'http://' + ip + ':8080/telemetry.json?usr=' + login + '&pwd=' + password;
+
+        var options = {
+            // url: 'http://192.168.73.4:8080/telemetry.json?usr=OrlanUser&pwd=OrlanPassword',
+            url: url,
+            headers: headers
+        };
+
+        // var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
+        // var data = JSON.parse(b);
+        //
+        //
+        // // var data = JSON.parse(body);
+        //
+        // var newarr = [];
+        // data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
+        // newarr = '{';
+        // for (let i = 0; i < data.length; i++) {
+        //     if (i === data.length - 1) {
+        //         newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
+        //     } else {
+        //         newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
+        //     }
+        //
+        //     if (data[i].Key == 'Recog24Hours') {
+        //         // violations_count = value;
+        //         let s = '{"passages":' + data[i].Value + '}';
+        //         res.send(s);
+        //     }
+        // }
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(body);
+
+                let s = '{"status":' + "active" + '}';
+                res.send(s);
+
+            } else {
+                console.error(error);
+                let s = '{"status":' + "inactive" + '}';
+                res.send(s);
+                //res.send(error);
+            }
+        }
+
+        request(options, callback);
 
     },
 
@@ -171,89 +208,121 @@ module.exports = {
             headers: headers
         };
 
-        var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
-        var data = JSON.parse(b);
-
-
-        // var data = JSON.parse(body);
-
-        var newarr = [];
-        data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
-        newarr = '{';
-        let s = '{{"status":' + '"active"'  + '},';
-        for (let i = 0; i < data.length; i++) {
-            if (i === data.length - 1) {
-                newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
-                if (data[i].Key === 'Recog24Hours') {
-                    // violations_count = value;
-                    s += '{"'+'passages'+'":' + data[i].Value + '}}';
-                }
-                if (data[i].Key === 'Recog1Hour') {
-                    // violations_count = value;
-                    s += '{"'+'violations'+'":' + data[i].Value + '}}';
-                }
-            } else {
-                newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
-                if (data[i].Key === 'Recog24Hours') {
-                    // violations_count = value;
-                    s += '{"'+'passages'+'":' + data[i].Value + '},';
-                }
-                if (data[i].Key === 'Recog1Hour') {
-                    // violations_count = value;
-                    s += '{"'+'violations'+'":' + data[i].Value + '},';
-                }
-
-            }
-
-        }
-        res.send(s);
-        // function callback(error, response, body) {
-        //     if (!error && response.statusCode === 200) {
-        //         console.log(body);
-        //
-        //         //var values = body;
-        //         var violations_count;
-        //
-        //         //just for test
-        //         var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
-        //         var data = JSON.parse(b);
+        // var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
+        // var data = JSON.parse(b);
         //
         //
-        //         // var data = JSON.parse(body);
+        // // var data = JSON.parse(body);
         //
-        //         var newarr = [];
-        //         data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
-        //         newarr = '{';
-        //         for (let i = 0; i < data.length; i++) {
-        //             if (i === data.length - 1) {
-        //                 newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
-        //             } else {
-        //                 newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
-        //             }
-        //
-        //             if (data[i].Key == 'Recog24Hours') {
-        //                 violations_count = value;
-        //                 let s = '{"violations":' + data[i].Value + '}';
-        //                 res.send(s);
-        //             }
+        // var newarr = [];
+        // data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
+        // newarr = '{';
+        // let s = '{{"status":' + '"active"' + '},';
+        // for (let i = 0; i < data.length; i++) {
+        //     if (i === data.length - 1) {
+        //         newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
+        //         if (data[i].Key === 'Recog24Hours') {
+        //             // violations_count = value;
+        //             s += '{"' + 'passages' + '":' + data[i].Value + '}}';
+        //         }
+        //         if (data[i].Key === 'Recog1Hour') {
+        //             // violations_count = value;
+        //             s += '{"' + 'violations' + '":' + data[i].Value + '}}';
+        //         }
+        //     } else {
+        //         newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
+        //         if (data[i].Key === 'Recog24Hours') {
+        //             // violations_count = value;
+        //             s += '{"' + 'passages' + '":' + data[i].Value + '},';
+        //         }
+        //         if (data[i].Key === 'Recog1Hour') {
+        //             // violations_count = value;
+        //             s += '{"' + 'violations' + '":' + data[i].Value + '},';
         //         }
         //
-        //     } else {
-        //         console.error(error);
-        //         res.send(error);
         //     }
-        // }
         //
-        // request(options, callback);
+        // }
+        // res.send(s);
+
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(body);
+
+                var data = JSON.parse(body);
+
+                var newarr = [];
+                data.forEach(element => newarr.push('{' + element.Key + ' : ' + element.Value + '}'));
+                newarr = '{';
+                let s = '{{"status":' + '"active"' + '},';
+                for (let i = 0; i < data.length; i++) {
+                    if (i === data.length - 1) {
+                        newarr += '{' + data[i].Key + ' : ' + data[i].Value + '}}';
+                        if (data[i].Key === 'Recog24Hours') {
+                            // violations_count = value;
+                            s += '{"' + 'passages' + '":' + data[i].Value + '}}';
+                        }
+                        if (data[i].Key === 'Recog1Hour') {
+                            // violations_count = value;
+                            s += '{"' + 'violations' + '":' + data[i].Value + '}}';
+                        }
+                    } else {
+                        newarr += '{' + data[i].Key + ' : ' + data[i].Value + '},';
+                        if (data[i].Key === 'Recog24Hours') {
+                            // violations_count = value;
+                            s += '{"' + 'passages' + '":' + data[i].Value + '},';
+                        }
+                        if (data[i].Key === 'Recog1Hour') {
+                            // violations_count = value;
+                            s += '{"' + 'violations' + '":' + data[i].Value + '},';
+                        }
+
+                    }
+
+                }
+                res.send(s);
+
+            } else {
+                console.error(error);
+                res.send(error);
+            }
+        }
+
+        request(options, callback);
 
     },
 
     getFullApiInfo: function getFullApiInfo(login, password, ip, timestampStart, timestampEnd, res) {
 
-        var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
-        var data = JSON.parse(b);
-        res.send(data);
+        // var b = '[{"Key":"UptimeSeconds","Value":"11240,546"},{"Key":"RadarInUse","Value":"true"},{"Key":"RadarFound","Value":"true"},{"Key":"RadarOk","Value":"true"},{"Key":"Temperature","Value":"22"},{"Key":"Voltage","Value":"14,1"},{"Key":"Recog24Hours","Value":"559"},{"Key":"Recog1Hour","Value":"32"}]';
+        // var data = JSON.parse(b);
+        // res.send(data);
 
+
+        var url = 'http://' + ip + ':8080/telemetry.json?usr=' + login + '&pwd=' + password;
+
+        var options = {
+            // url: 'http://192.168.73.4:8080/telemetry.json?usr=OrlanUser&pwd=OrlanPassword',
+            url: url,
+            headers: headers
+        };
+
+        function callback(error, response, body) {
+            if (!error && response.statusCode === 200) {
+                console.log(body);
+
+                var data = JSON.parse(body);
+                res.send(data);
+
+            } else {
+                console.error(error);
+                let s = '{"status":' + "inactive" + '}';
+                res.send(s);
+            }
+        }
+
+        request(options, callback);
     }
 
 };
