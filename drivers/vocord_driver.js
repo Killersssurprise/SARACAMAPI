299e75dd92bde411337f14ca6c0d183f676afb65
Killersssurprise <killersssurprise@gmail.com>
2021-02-20 00:24:44 +0300

    Test

--- drivers/vocord_driver.js
+++ drivers/vocord_driver.js
@@ -30,30 +30,30 @@ module.exports = {
         function callback(error, response, body) {
             if (!error && response.statusCode == 200) {
                 console.log(body);
-                res.send(body);
+
                 //var data = JSON.parse(body);
 
-                // var pk = JSON.parse(body);
-                // // var pk = body;
-                // var modulusHex = base64ToHex(pk.Modulus);
-                // var exponentHex = base64ToHex(pk.Exponent);
-                //
-                // //console.log(pk.Modulus);
-                // //console.log(pk.Exponent);
-                //
-                // var rsa = new rs.Key();
-                // rsa.setPublic(modulusHex, exponentHex);
-                //
-                // var answer;
-                // var input = "OrlanPassword";
-                // if (Array.isArray(input)) {
-                //     answer = input.map(function(x) { return encryptImpl(rsa, x); });
-                // } else {
-                //     answer = encryptImpl(rsa, input);
-                // }
-                //
-                //
-                // makeLoginRequest(res,answer);
+                var pk = JSON.parse(body);
+                // var pk = body;
+                var modulusHex = base64ToHex(pk.Modulus);
+                var exponentHex = base64ToHex(pk.Exponent);
+
+                //console.log(pk.Modulus);
+                //console.log(pk.Exponent);
+
+                var rsa = new rs.Key();
+                rsa.setPublic(modulusHex, exponentHex);
+
+                var answer;
+                var input = "OrlanPassword";
+                if (Array.isArray(input)) {
+                    answer = input.map(function(x) { return encryptImpl(rsa, x); });
+                } else {
+                    answer = encryptImpl(rsa, input);
+                }
+
+
+                makeLoginRequest(res,answer);
 
 
 
@@ -450,7 +450,9 @@ function makeLoginRequest(res, passwordCoded){
         'Accept-Language': 'en-US,en;q=0.9,ru;q=0.8'
     };
 
-    var dataString = 'grant_type=password&username=admin&password='+encodeURIComponent(passwordCoded);
+    // var dataString = 'grant_type=password&username=admin&password='+encodeURIComponent(passwordCoded);
+    var dataString = 'grant_type=password&username=admin&password=SeF%2Fex71FshzlW1%2BE9Tu4f0tR2FIe1NgBlNWKc2eakSs2FHRcAvlQHWtAQAq5nwYhlL%2BqsAbuXs0tKLCEzA8zC5B7jG%2Fc9kKiOJcW3fX8DBwtfNpR41l80Ujj1rMN5i8e86v0ylowqpSPaBSA82B8zdEZvSm0QNbWGB3XICYDgM%3D';
+
 
     var options = {
         url: 'http://192.168.40.26/MonoblockService/token',
