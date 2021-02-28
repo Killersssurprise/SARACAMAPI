df28d51478f500e5ba8772fdc4ee472ebfa1098e
Killersssurprise <killersssurprise@gmail.com>
2021-02-28 22:39:25 +0300

    Fixed bug with cordon

--- drivers/kordon_driver.js
+++ drivers/kordon_driver.js
@@ -292,8 +292,8 @@ module.exports = {
         console.log("port is: " + _port);
 
         var options = {
-            url: 'http://' + ip + ':' + _port + '/api11.php',
-            // host: ip,
+            // url: 'http://' + ip + ':' + _port + '/api11.php',
+            host: ip,
             // port: _port,
             // path: '/api11.php',
             method: 'POST',
