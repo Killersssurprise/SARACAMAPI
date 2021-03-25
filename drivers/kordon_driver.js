2cffb37aed2c7590ba66e103323fa071a4a3bf54
Killersssurprise <killersssurprise@gmail.com>
2021-03-25 13:11:41 +0300

    Cordon try

--- drivers/kordon_driver.js
+++ drivers/kordon_driver.js
@@ -258,7 +258,7 @@ module.exports = {
         var snmpsession = new snmp.Session({host: ip, port: 161, community: 'special'});
         //NET-SNMP-MIB::netSnmp.99.1.1.1.3.18.72.101.97.108.116.104.58.32.80.67.32.118.111.108.116.97.103.101
         // snmpsession.get({oid: ['NET-SNMP-MIB::netSnmp',99, 1, 1, 1, 3, 18, 72, 101, 97, 108, 116, 104, 58, 32, 80, 67, 32, 118, 111,108,116,97,103,101]}, function (error, varbinds) {
-        snmpsession.get({oid: [99, 1, 1, 1, 3, 18, 72, 101, 97, 108, 116, 104, 58, 32, 80, 67, 32, 118, 111,108,116,97,103,101]}, function (error, varbinds) {
+        snmpsession.get({oid: ['NET-SNMP-MIB::netSnmp.99.1.1.1.3.18.72.101.97.108.116.104.58.32.80.67.32.118.111.108.116.97.103.101']}, function (error, varbinds) {
             if (error) {
                 console.log('Fail :(');
             } else {
