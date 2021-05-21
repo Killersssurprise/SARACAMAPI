c4aae3a2f024b8c5116d149c90c02c2099ea2328
Killersssurprise <killersssurprise@gmail.com>
2021-05-21 15:16:40 +0300

    Fixed bug with cordon

--- drivers/kordon_driver_alternative.js
+++ drivers/kordon_driver_alternative.js
@@ -289,37 +289,6 @@ module.exports = {
 
         let pingMS1 = Date.now();
 
-
-        var data = JSON.stringify({
-            "auth": {
-                "login": login,
-                "password": password
-            },
-            "request": {
-                "job": job,
-                "getStats": {
-                    "timestampStart": timestampStart,
-                    "timestampEnd": timestampEnd,
-                    "speedThresholds": [
-                        {
-                            "name": "Превышение на 20", "min": 23, "max": 43
-                        },
-                        {
-                            "name": "Превышение на 40",
-                            "min": 43, "max": 63
-                        },
-                        {
-                            "name": "Превышение на 60", "min": 63, "max": 83
-                        },
-                        {
-                            "name": "Превышение на 80", "min": 83, "max": 0
-                        }
-                    ],
-                    "showInfo": true
-                }
-            }
-        });
-
         let _port = '80';
 
         if (port !== '' && typeof port !== 'undefined' && port !== null) {
@@ -342,11 +311,14 @@ module.exports = {
             headers: headersDataRequest,
             body: dataStringDataRequest
         };
+        var ddd = '';
 
         function callbackDataRequest(error, response, body) {
             if (!error && response.statusCode === 200) {
                 var data = body;
-                console.log("callbackDataRequest"+body);
+
+                console.log("callbackDataRequest" + body);
+                ddd = body;
                 // res.send(body);
                 return body;
             }
@@ -355,7 +327,39 @@ module.exports = {
         // request(options, callback);
         request(optionsDataRequest, callbackDataRequest).then(function (body) {
 
-            var optionsMainData = {
+
+            var data = JSON.stringify({
+                "auth": {
+                    "login": login,
+                    "password": password
+                },
+                "request": {
+                    "job": job,
+                    "getStats": {
+                        "timestampStart": timestampStart,
+                        "timestampEnd": timestampEnd,
+                        "speedThresholds": [
+                            {
+                                "name": "Превышение на 20", "min": 23, "max": 43
+                            },
+                            {
+                                "name": "Превышение на 40",
+                                "min": 43, "max": 63
+                            },
+                            {
+                                "name": "Превышение на 60", "min": 63, "max": 83
+                            },
+                            {
+                                "name": "Превышение на 80", "min": 83, "max": 0
+                            }
+                        ],
+                        "showInfo": true
+                    }
+                }
+            });
+
+
+            var options = {
                 // url: 'http://' + ip + ':' + _port + '/api11.php',
                 host: ip,
                 // port: _port,
@@ -367,86 +371,56 @@ module.exports = {
                 }
 
             };
-
+            // console.log("url " + 'http://' + ip + ':' + _port + '/api11.php');
             var answ = '';
+            try {
+                var httpreq = http.request(options, function (response) {
+                    //response.setEncoding('utf8');
+                    response.on('data', function (chunk) {
+                        //console.log(chunk);
+                        answ += chunk;
+                    }).on('end', function () {
+                        var str = JSON.parse(answ);
 
-            var ddd = body;
+                        //let s = '{"violations":' + JSON.stringify(str['getStats']['violation']['total']) + '}';
+                        // let violations = '{"violations":' + JSON.stringify(str['getStats']['violation']['total']) + '}';
+                        // let passages = '{"passages":' + JSON.stringify(str['getStats']['common']['total']) + '}';
+                        // let isActive = '{"status":' + "active" + '}';
 
+                        let pingMS2 = Date.now();
 
-            function callbackMainData(error, response, body) {
-                if (!error && response.statusCode === 200) {
-                    // var data = body;
-                    console.log("callbackDataRequest"+body);
-                    // res.send(body);
-                    var str = JSON.parse(body);
-                    // return str;
-                    //
-                    //
-                    let pingMS2 = Date.now();
+                        var data = {
+                            violations: JSON.stringify(str['getStats']['violation']['total']),
+                            passages: JSON.stringify(str['getStats']['common']['total']),
+                            status: 'active',
+                            ping: (pingMS2 - pingMS1),
+                            voltage: (snmp_voltage / 10),
+                            data: ddd
+                        };
 
-                    var data = {
-                        violations: JSON.stringify(str['getStats']['violation']['total']),
-                        passages: JSON.stringify(str['getStats']['common']['total']),
-                        status: 'active',
-                        ping: (pingMS2 - pingMS1),
-                        voltage: (snmp_voltage / 10),
-                        data: ddd
-                    };
+                        let answer = JSON.stringify(data);
 
-                    let answer = JSON.stringify(data);
-                    console.log("callbackMainData"+body);
-                    // return str;
-                    res.send(answer);
+                        //console.log(str);
+                        //res.send(answer);
+                        // answ = answer;
+                        res.send(answer);
+                        //res.removeAllListeners('data');
+                        // httpreq.end();
 
-                    // return body;
-                }else{
-                    let s = '{"status":' + "inactive" + '}';
-                    //res.send(s);
-                    console.error(error.stack);
-                    res.send(s);
-                }
+                    }).on('error', (err) => {
+                        let s = '{"status":' + "inactive" + '}';
+                        //res.send(s);
+                        console.error(err.stack);
+                        // answ = s
+                        res.send(s);
+                    });
+                });
+                httpreq.write(data);
+                httpreq.end();
+            } catch (e) {
+
+                console.log(e);
             }
-
-            // function callbackMainData(response) {
-            //     //response.setEncoding('utf8');
-            //     response.on('data', function (chunk) {
-            //         //console.log(chunk);
-            //         answ += chunk;
-            //     }).on('end', function () {
-            //         var str = JSON.parse(answ);
-            //         // return str;
-            //         //
-            //         //
-            //         let pingMS2 = Date.now();
-            //
-            //         var data = {
-            //             violations: JSON.stringify(str['getStats']['violation']['total']),
-            //             passages: JSON.stringify(str['getStats']['common']['total']),
-            //             status: 'active',
-            //             ping: (pingMS2 - pingMS1),
-            //             voltage: (snmp_voltage / 10),
-            //             data: ddd
-            //         };
-            //
-            //         let answer = JSON.stringify(data);
-            //         console.log("callbackMainData"+body);
-            //         // return str;
-            //         res.send(answer);
-            //
-            //     }).on('error', (err) => {
-            //         let s = '{"status":' + "inactive" + '}';
-            //         //res.send(s);
-            //         console.error(err.stack);
-            //         res.send(s);
-            //         // return s;
-            //     });
-            // }
-
-            // var httpreq = http.request(options,);
-            // httpreq.write(data);
-            // httpreq.end();
-
-            request(optionsMainData, callbackMainData());
 
         });
 
