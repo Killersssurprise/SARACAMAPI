f9c4afcfdbafbded09ac230d4c6f6b688e605431
Killersssurprise <killersssurprise@gmail.com>
2021-05-25 13:12:08 +0300

    Fixed bug with cordon

--- drivers/kordon_driver_alternative.js
+++ drivers/kordon_driver_alternative.js
@@ -337,22 +337,22 @@ module.exports = {
                 return body;
             } else {
 
-                var authErrMsg = '{"authorization":false,"error":"401"}';
-                var output = '';
+                // var authErrMsg = '{"authorization":false,"error":"401"}';
+                // var output = '';
+                //
+                //
+                // if ((error && error.toString().includes(authErrMsg)) || (body && body.toString().includes(authErrMsg))) {
+                //     output = 'Ошибка авторизации! Не верный логин или пароль!';
+                // } else {
+                //     output = error;
+                // }
+                //
+                // var errData = {
+                //     error: output,
+                //     code: response.statusCode,
+                // };
 
-
-                if ((error && error.toString().includes(authErrMsg)) || (body && body.toString().includes(authErrMsg))) {
-                    output = 'Ошибка авторизации! Не верный логин или пароль!';
-                } else {
-                    output = error;
-                }
-
-                var errData = {
-                    error: output,
-                    code: response.statusCode,
-                };
-
-                let errAnswer = JSON.stringify(errData);
+                let errAnswer = utils.getErrorMessage(error, response, body);
 
                 utils.foo();
                 res.send(errAnswer);
--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,26 @@
 module.exports = {
-    foo: function () {
+    getErrorMessage: function (error, response, body) {
         // whatever
         console.log("Hello from utils!")
+
+        var authErrMsg = '{"authorization":false,"error":"401"}';
+        var output = '';
+
+
+        if ((error && error.toString().includes(authErrMsg)) || (body && body.toString().includes(authErrMsg))) {
+            output = 'Ошибка авторизации! Не верный логин или пароль!';
+        } else {
+            output = error;
+        }
+
+        var errData = {
+            error: output,
+            code: response.statusCode,
+        };
+
+        let errAnswer = JSON.stringify(errData);
+
+        return errAnswer;
     },
     bar: function () {
         // whatever
