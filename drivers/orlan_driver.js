eb1c27f8eb681d3c1c17ba4fcd6acb3b9d76363c
Killersssurprise <killersssurprise@gmail.com>
2021-05-25 13:44:22 +0300

    Fixed bug with cordon

--- drivers/orlan_driver.js
+++ drivers/orlan_driver.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var http = require('http');
 const jsonParser = express.json();
 var request = require('request');
+var utils = require('../utils/utils');
 
 var headers = {
     'Content-Length': '1000'
@@ -140,10 +141,12 @@ module.exports = {
                 res.send(s);
 
             } else {
-                console.error(error);
-                let s = '{"status":' + "inactive" + '}';
-                res.send(s);
-                //res.send(error);
+                // console.error(error);
+                // let s = '{"status":' + "inactive" + '}';
+                // res.send(s);
+                // //res.send(error);
+                let errAnswer = utils.getErrorMessage(error, response, body);
+                res.send(errAnswer);
             }
         }
 
@@ -309,9 +312,12 @@ module.exports = {
                 res.send(data);
 
             } else {
-                console.error(error);
-                let s = '{"status":' + "inactive" + '}';
-                res.send(s);
+                // console.error(error);
+                // let s = '{"status":' + "inactive" + '}';
+                // res.send(s);
+
+                let errAnswer = utils.getErrorMessage(error, response, body);
+                res.send(errAnswer);
             }
         }
 
