8cdbf07571bcdb899c4c5085ae674caf88ca34c5
Killersssurprise <killersssurprise@gmail.com>
2021-06-09 21:14:16 +0300

    Fixed bug with cordon

--- drivers/orlan_driver.js
+++ drivers/orlan_driver.js
@@ -297,7 +297,7 @@ module.exports = {
                 // console.log("Response: "+response);
                 // var data = JSON.parse(body);
                 // var data = JSON.stringify(body);
-                var data = body.body;
+                var data = JSON.parse(body);
                 console.log(data);
                 // var v;
                 // var p;
