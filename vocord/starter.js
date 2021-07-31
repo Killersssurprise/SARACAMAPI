3078b2df6cee52baa7e419b59efe30adfa31ea80
Killersssurprise <killersssurprise@gmail.com>
2021-07-31 20:23:24 +0300

    Fixed bug with cordon

--- vocord/starter.js
+++ vocord/starter.js
@@ -1,32 +1,39 @@
 // global.atob = require("atob");
-
-
 var http = require('http');
+var httpProxy = require('http-proxy');
 
-http.createServer(onRequest).listen(3000);
+var proxy = httpProxy.createProxyServer({});
 
-function onRequest(client_req, client_res) {
-    console.log('serve: ' + client_req.url);
+http.createServer(function(req, res) {
+    proxy.web(req, res, { target: 'http://192.168.40.10:18000/video/data.mjpg' });
+}).listen(3000);
 
-    var options = {
-        hostname: 'http://192.168.40.10:18000/video/data.mjpg',
-        port: 80,
-        path: client_req.url,
-        method: client_req.method,
-        headers: client_req.headers
-    };
-
-    var proxy = http.request(options, function (res) {
-        client_res.writeHead(res.statusCode, res.headers)
-        res.pipe(client_res, {
-            end: true
-        });
-    });
-
-    client_req.pipe(proxy, {
-        end: true
-    });
-}
+// var http = require('http');
+//
+// http.createServer(onRequest).listen(3000);
+//
+// function onRequest(client_req, client_res) {
+//     console.log('serve: ' + client_req.url);
+//
+//     var options = {
+//         hostname: 'http://192.168.40.10:18000/video/data.mjpg',
+//         port: 80,
+//         path: client_req.url,
+//         method: client_req.method,
+//         headers: client_req.headers
+//     };
+//
+//     var proxy = http.request(options, function (res) {
+//         client_res.writeHead(res.statusCode, res.headers)
+//         res.pipe(client_res, {
+//             end: true
+//         });
+//     });
+//
+//     client_req.pipe(proxy, {
+//         end: true
+//     });
+// }
 
 
 // var http = require('http');
