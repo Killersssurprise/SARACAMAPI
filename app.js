ff4957c3f94e87c96b4381d86cdb1f9be99a59ae
Killersssurprise <killersssurprise@gmail.com>
2021-07-31 19:02:23 +0300

    test req

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
+var http = require('http');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -29,13 +28,13 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 ////
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 /////
@@ -52,20 +51,35 @@ app.use('/vocord', vocord);
 app.use('/test', test);
 app.use('/video', video);
 
+// let target = 'http://direct.fipradio.fr/live/fip-midfi.mp3';
+let target = 'http://192.168.40.10:18000/video/data.mjpg';
+// figure out 'real' target if the server returns a 302 (redirect)
+http.get(target, resp => {
+    if (resp.statusCode === 302) {
+        target = resp.headers.location;
+    }
+});
+
+app.use(express.static('dist'));
+
+app.get('/stream', (req, res) => {
+    req.pipe(request.get(target)).pipe(res);
+});
+
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
