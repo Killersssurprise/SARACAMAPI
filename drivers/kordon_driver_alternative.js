524ec3e5fdd46c068eadcca66f0df0cb7e9201e4
Killersssurprise <killersssurprise@gmail.com>
2021-08-14 21:43:54 +0300

    Fixed bug with cordon

--- drivers/kordon_driver_alternative.js
+++ drivers/kordon_driver_alternative.js
@@ -593,93 +593,18 @@ module.exports = {
 
     getVideo: function getVideo(login, password, ip, port, timestampStart, timestampEnd, res) {
 
-        // var data = JSON.stringify({
-        //     "auth": {
-        //         "login": login,
-        //         "password": password
-        //     },
-        //     "request": {
-        //         "job": job,
-        //         "getStats": {
-        //             "timestampStart": timestampStart,
-        //             "timestampEnd": timestampEnd,
-        //             "speedThresholds": [
-        //                 {
-        //                     "name": "Превышение на 20", "min": 23, "max": 43
-        //                 },
-        //                 {
-        //                     "name": "Превышение на 40",
-        //                     "min": 43, "max": 63
-        //                 },
-        //                 {
-        //                     "name": "Превышение на 60", "min": 63, "max": 83
-        //                 },
-        //                 {
-        //                     "name": "Превышение на 80", "min": 83, "max": 0
-        //                 }
-        //             ],
-        //             "showInfo": true
-        //         }
-        //     }
-        // });
-        //
-        // let _port = '80';
-        //
-        // if (port !== '' && typeof port !== 'undefined' && port !== null) {
-        //     _port = port;
-        // }
-        //
-        // var options = {
-        //     url: 'http://' + ip + ':' + _port + '/api11.php',
-        //     // host: ip,
-        //     // port: _port,
-        //     // path: '/api11.php',
-        //     method: 'POST',
-        //     headers: {
-        //         'Content-Type': 'application/json',
-        //         'Content-Length': Buffer.byteLength(data)
-        //     }
-        //
-        // };
-        // var answ = '';
-        // try {
-        //     var httpreq = http.request(options, function (response) {
-        //         //response.setEncoding('utf8');
-        //         response.on('data', function (chunk) {
-        //             //console.log(chunk);
-        //             answ += chunk;
-        //         }).on('end', function () {
-        //             var str = JSON.parse(answ);
-        //
-        //             //let s = '{"violations":' + JSON.stringify(str['getStats']['violation']['total']) + '}';
-        //             //console.log(str);
-        //             //res.send(str);
-        //             //answ = str;
-        //             res.send(str);
-        //         }).on('error', (err) => {
-        //             let s = '{"status":' + "inactive" + '}';
-        //             console.error(err.stack);
-        //             //answ = s;
-        //             res.send(s);
-        //         });
-        //     });
-        //     httpreq.write(data);
-        //     httpreq.end();
-        // } catch (e) {
-        //
-        //     console.log(e);
-        // }
+        // http://192.168.40.130:84/140F7FC9-4C38-45F1-9E71-538F0C99C458/medium/560.jpg?id=1627758275951
 
         var url;
-        url = 'http://192.168.40.10';
+        url = 'http://192.168.40.130';
         var parsedHost = url.split('/').splice(2).splice(0, 1).join('/')
         // var parsedPort;
 
         var options = {
             hostname: parsedHost,
-            port: 18000,
+            port: 84,
             // path: clientRequest.url,
-            path:'/video/frame.jpeg?frame=SEQUENCE_COUNTER_TO_PREVENT_CACHING',
+            path:'/140F7FC9-4C38-45F1-9E71-538F0C99C458/medium/560.jpg?id=1627758275951',
             // method: clientRequest.method,
             method: 'GET',
             headers: {
