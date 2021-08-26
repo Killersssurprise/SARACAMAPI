b06d59bccf3f56b6871f56634e44d8e122bb617c
Killersssurprise <killersssurprise@gmail.com>
2021-08-26 17:27:42 +0300

    Fixed bug with cordon

--- drivers/kordon_driver_alternative.js
+++ drivers/kordon_driver_alternative.js
@@ -6,8 +6,19 @@ var exec = require('child_process').exec;
 var rs = require("node-bignumber");
 var request = require('request-promise');
 var utils = require('../utils/utils');
+var app = express();
+const {proxy, scriptUrl} = require('rtsp-relay')(app);
 
 //curl --location --request POST "http://192.168.72.3:80/api11.php" --header "Content-Type: application/json" --data '{ "auth": {"login": "admin","password": "8aHrgDKW"}, "request": { "job": "getDeviceInfo"}}'
+
+const handler = proxy({
+    // url: `rtsp://admin:admin@10.0.1.2:554/feed`,
+    url: `rtsp://admin:8aHrgDKW@192.168.72.9:554`,
+    // if your RTSP stream need credentials, include them in the URL as above
+    verbose: false,
+});
+
+app.ws('/api/stream', handler);
 
 
 module.exports = {
@@ -589,94 +600,102 @@ module.exports = {
 
         return answ;
 
-    }
+    },
 
-    // getVideo: function getVideo(login, password, ip, port, timestampStart, timestampEnd, res) {
-    //
-    //     // http://192.168.40.130:84/140F7FC9-4C38-45F1-9E71-538F0C99C458/medium/560.jpg?id=1627758275951
-    //
-    //     /***** запрос на получение channelId*/
-    //
-    //     // var request = require('request');
-    //     //
-    //     // var headers = {
-    //     //     'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:91.0) Gecko/20100101 Firefox/91.0',
-    //     //     'Accept': 'application/json, text/plain, */*',
-    //     //     'Accept-Language': 'ru-RU,ru;q=0.8,en-US;q=0.5,en;q=0.3',
-    //     //     'If-Modified-Since': 'Mon, 26 Jul 1997 05:00:00 GMT',
-    //     //     'Cache-Control': 'no-cache',
-    //     //     'Pragma': 'no-cache',
-    //     //     'Authorization': 'Bearer qJJbLc9ThmH0A64Pa1SO1nkXznewnC5OSniBvxgMiweQbQiNaGNZ7eS0X-fERC6tbzeEAnv2VP86Kxi9Pww0JR-URjBDCJZzQwO41ZLvPlhp-wNqZwBwRmgverBPU1l9MGVqLn--lnO4u9N-GPJiozoMdRJI_eE1Dc7HplX4gESpQw2UvQ2VTDmrmECeXKyjT_VvcXNiMY1IwKwMi-egDyYw0nwk_3F7Oa_tY6sljrg',
-    //     //     'DNT': '1',
-    //     //     'Connection': 'keep-alive',
-    //     //     'Referer': 'http://192.168.40.130/'
-    //     // };
-    //     //
-    //     // var options = {
-    //     //     url: 'http://192.168.40.130/MonoblockService/api/camera/GetCamerasForNotification',
-    //     //     headers: headers
-    //     // };
-    //     //
-    //     // function callback(error, response, body) {
-    //     //     if (!error && response.statusCode == 200) {
-    //     //         console.log(body);
-    //     //     }
-    //     // }
-    //     //
-    //     // request(options, callback);
-    //
-    //     /*****/
-    //
-    //     var url;
-    //     url = 'http://192.168.40.130';
-    //     var parsedHost = url.split('/').splice(2).splice(0, 1).join('/')
-    //     // var parsedPort;
-    //
-    //     var options = {
-    //         hostname: parsedHost,
-    //         port: 84,
-    //         // path: clientRequest.url,
-    //         path:'/140F7FC9-4C38-45F1-9E71-538F0C99C458/medium/560.jpg?id=1627758275951',
-    //         // method: clientRequest.method,
-    //         method: 'GET',
-    //         headers: {
-    //             'User-Agent': 'auto-request-bot',
-    //             'Content-Type':'image/jpeg',
-    //             'Connection':'keep-alive',
-    //             'Keep-Alive':'timeout=5',
-    //             'Transfer-Encoding':'chunked'
-    //         }
-    //     };
-    //
-    //     var serverRequest = http.request(options, function(serverResponse) {
-    //         var body = '';
-    //         if (String(serverResponse.headers['content-type']).indexOf('text/html') !== -1) {
-    //             serverResponse.on('data', function(chunk) {
-    //                 body += chunk;
-    //             });
-    //
-    //             serverResponse.on('end', function() {
-    //                 // Make changes to HTML files when they're done being read.
-    //                 body = body.replace(`example`, `Cat!` );
-    //
-    //                 res.writeHead(serverResponse.statusCode, serverResponse.headers);
-    //                 res.end(body);
-    //             });
-    //         }
-    //         else {
-    //             serverResponse.pipe(res, {
-    //                 end: true
-    //             });
-    //             res.contentType(serverResponse.headers['content-type'])
-    //         }
-    //     });
-    //
-    //     serverRequest.end();
-    //
-    //     var answ = '';
-    //     return answ;
-    //
-    // }
+    getVideo: function getVideo(login, password, ip, port, timestampStart, timestampEnd, res) {
+
+
+        // var data = JSON.stringify({
+        //     "auth": {
+        //         "login": login,
+        //         "password": password
+        //     },
+        //     "request": {
+        //         "job": job,
+        //         "getStats": {
+        //             "timestampStart": timestampStart,
+        //             "timestampEnd": timestampEnd,
+        //             "speedThresholds": [
+        //                 {
+        //                     "name": "Превышение на 20", "min": 23, "max": 43
+        //                 },
+        //                 {
+        //                     "name": "Превышение на 40",
+        //                     "min": 43, "max": 63
+        //                 },
+        //                 {
+        //                     "name": "Превышение на 60", "min": 63, "max": 83
+        //                 },
+        //                 {
+        //                     "name": "Превышение на 80", "min": 83, "max": 0
+        //                 }
+        //             ],
+        //             "showInfo": true
+        //         }
+        //     }
+        // });
+        //
+        // let _port = '80';
+        //
+        // if (port !== '' && typeof port !== 'undefined' && port !== null) {
+        //     _port = port;
+        // }
+        //
+        // var options = {
+        //     url: 'http://' + ip + ':' + _port + '/api11.php',
+        //     // host: ip,
+        //     // port: _port,
+        //     // path: '/api11.php',
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         'Content-Length': Buffer.byteLength(data)
+        //     }
+        //
+        // };
+        // var answ = '';
+        // try {
+        //     var httpreq = http.request(options, function (response) {
+        //         //response.setEncoding('utf8');
+        //         response.on('data', function (chunk) {
+        //             //console.log(chunk);
+        //             answ += chunk;
+        //         }).on('end', function () {
+        //             var str = JSON.parse(answ);
+        //
+        //             //let s = '{"violations":' + JSON.stringify(str['getStats']['violation']['total']) + '}';
+        //             //console.log(str);
+        //             //res.send(str);
+        //             //answ = str;
+        //             res.send(str);
+        //         }).on('error', (err) => {
+        //             let s = '{"status":' + "inactive" + '}';
+        //             console.error(err.stack);
+        //             //answ = s;
+        //             res.send(s);
+        //         });
+        //     });
+        //     httpreq.write(data);
+        //     httpreq.end();
+        // } catch (e) {
+        //
+        //     console.log(e);
+        // }
+
+        res.send(`
+  <canvas id='canvas'></canvas>
+
+  <script src='${scriptUrl}'></script>
+  <script>
+    loadPlayer({
+      url: 'ws://' + location.host + '/api/stream',
+      canvas: document.getElementById('canvas')
+    });
+  </script>
+`);
+        // return answ;
+
+    }
 
 
 };
