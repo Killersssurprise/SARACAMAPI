fa41b4dee171be003bf6551bc785a53e96499247
Killersssurprise <killersssurprise@gmail.com>
2021-08-27 13:25:39 +0300

    Fixed bug with cordon

--- vocord/starter.js
+++ vocord/starter.js
@@ -11,7 +11,17 @@ const handler = proxy({
 });
 
 // the endpoint our RTSP uses
-app.ws('/api/stream', handler);
+app.ws('/api/stream',function(ws, req, next) {
+    ws.on('message', function(msg) {
+        console.log('msg:', msg)
+    })
+}, handler);
+
+// app.ws('/', function(ws, req, next) {
+//     ws.on('message', function(msg) {
+//         console.log('msg:', msg)
+//     })
+// });
 
 // this is an example html page to view the stream
 app.get('/', (req, res) =>
